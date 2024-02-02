7abcef137c757bcfea8f142e32820dbdef4c986e
m88deng <melideng123@gmail.com>
2024-02-02 14:58:25 -0500

    add credentials for login session

--- bro.py
+++ bro.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import refinitiv.dataplatform as rdp
+
+import refinitiv.dataplatform as rdp
+import datetime
+
+
+APP_KEY = '7859a2c4d2a74b0cb21368d1fd1fb6281248058c'
+
+rdp.open_desktop_session(APP_KEY)
