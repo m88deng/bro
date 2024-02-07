137080cff8670c4d065581edd49903ac19b0584f
m88deng <melideng123@gmail.com>
2024-02-06 20:09:08 -0500

    add ticker

--- bro.py
+++
@@ -1,9 +0,0 @@
-import refinitiv.dataplatform as rdp
-
-import refinitiv.dataplatform as rdp
-import datetime
-
-
-APP_KEY = '7859a2c4d2a74b0cb21368d1fd1fb6281248058c'
-
-rdp.open_desktop_session(APP_KEY)
